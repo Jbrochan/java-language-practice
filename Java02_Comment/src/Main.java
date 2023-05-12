4da37cfd1313743fe8e5ed4eb79245ebe1b45d3d
Jbrochan <hyeongchanj@gmail.com>
2023-05-12 19:24:16 +0900

    feat : Add "Comment" project
    주석 학습 완료

---
+++ Java02_Comment/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Comment for one line");
+        /*
+        System.out.println("Comment for multiple lines);
+         */
+    }
+}

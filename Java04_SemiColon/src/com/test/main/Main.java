a5deba4b71ba7b817d1f13b23f2a3dc543e163c5
Jbrochan <hyeongchanj@gmail.com>
2023-05-13 15:04:38 +0900

    feat : Add Semicolon project

---
+++ Java04_SemiColon/src/com/test/main/Main.java
@@ -0,0 +1,8 @@
+package com.test.main;
+
+public class Main {
+    public static void main(String[] args){
+        // 자바는 문장의 마지막에 반드시 세미콜론(;)을 찍어줘야 한다.
+        System.out.println("세미콜론 있음");
+    }
+}
